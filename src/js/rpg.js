8307a7132d90c68ec39f14f713edd3e303d03e0f
Richard <charichard09@gmail.com>
2023-02-02 08:23:44 -0800

    add ui and business logic to add points to character

--- src/js/rpg.js
+++ src/js/rpg.js
@@ -11,74 +11,73 @@ const storeState = () => {
 
 const stateControl = storeState();
 
-const changeState = (prop) => {
+const changeStateNum = (prop) => {
   return (value) => {
     return (state) => ({
       ...state,
-      [prop] : (state[prop] || 0) + value
+      [prop] : (state[prop] || 0) + value,
     })
   }
 }
 
-const replaceStateValue = (prop) => {
+const changeStateString = (prop) => {
   return (value) => {
     return (state) => ({
       ...state,
-      [prop] : value
+      [prop] : (state[prop] || "") + value,
     })
   }
 }
-
-
-
-function isString(input) {}
-
-// init character
-// const character = stateControl(changeState("name")(1));
-// character("BigDeeps")
 
 // add character attribute functions
-const addActive = stateControl(changeState("active")(1));
-const addGoofball = stateControl(changeState("goofball")(1));
-const addCreative = stateControl(changeState("creative")(1));
-const addGenius = stateControl(changeState("genius")(1));
-const addSelfAssured = stateControl(changeState("selfAssured")(1));
-const addMean = stateControl(changeState("mean")(1));
-
-function playGame() {
-  // create & store character in stateControl
-  character;
-  // let counter = 0;
-  // recursively listen for user input to add attribute values
-  // based on user input update character attributes
-  addActive;
-  addGoofball;
-  addCreative;
-  addGenius;
-  addSelfAssured;
-  addMean;
-
-  return stateControl();
-}
+// const addActive = changeState("active")(1);
+// const addGoofball = changeState("goofball")(1);
+// const addCreative = changeState("creative")(1);
+// const addGenius = changeState("genius")(1);
+// const addSelfAssured = changeState("selfAssured")(1);
+// const addMean = changeState("mean")(1);
+// const addBeginnerPoints = changeState("BeginnerPoints")(1);
 
 
 // UI logic
+function addPoints() {
+  // document.getElementById("add-active").onclick = () => {
+  //   // removed stateControl from add variables, call stateControl here b/c reasons. also add to beginner points
+  //   const newState = { ...stateControl(addActive), ...stateControl(addBeginnerPoints) };
+  //   document.getElementById("active").innerText = `${newState.active}`;
+  // }
+
+  document.getElementById("add-points").onclick = (event) => {
+    if (event.target.id === "add-points") {
+      return;
+    }
+    const attribute = event.target.value;
+    const newState = { ...stateControl(changeStateNum(attribute)(1)) }
+    document.getElementById(attribute).innerText = `${newState[attribute]}`;
+  }
+}
+
 window.onload = function() {
   // e.preventDefault();
   document.getElementById("create-char").onsubmit = (e) => {
     e.preventDefault();
-    let charName = document.getElementById("char-name").innerText;
-    const character = { ...stateControl(), name: charName };
+    // change innerText to .value
+    let charName = document.getElementById("char-name").value;
+    const character = { ...stateControl(changeStateString("name")(charName)) };
     document.getElementById("name").innerText = `Name: ${character.name}`;
   }
 
-  document.getElementById("add-active").onclick = () => {
-    addActive();
-    console.log(character);
+  addPoints();
+
+  document.getElementById("finalize").onclick = () => {
+    const newState = { ...stateControl() };
+    document.getElementById("character-name").innerText = `${newState.name}`;
+    document.getElementById("char-active").innerText = `${newState.active}`;
+    document.getElementById("char-goofball").innerText = `${newState.goofball}`;
+    document.getElementById("char-creative").innerText = `${newState.creative}`;
+    document.getElementById("char-genius").innerText = `${newState.genius}`;
+    document.getElementById("char-selfAssured").innerText = `${newState.selfAssured}`;
+    document.getElementById("char-mean").innerText = `${newState.mean}`;
   }
 }
 
-
-// ALWAYS STAYS AT THE BOTTOM
-// STARTS FROM THE BOTTOM AND STAYS THERE
-module.exports = playGame;
