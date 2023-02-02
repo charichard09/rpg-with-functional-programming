70ec1c9f9291c94b9a495610452f15691e752d8e
Richard <charichard09@gmail.com>
2023-02-02 10:11:54 -0800

    add point cap
    Co-authored-by: Kirsten Opstad <kirsten.opstad@gmail.com>
    Co-authored-by: Aitana Shough <aeshough@gmail.com>
    Co-authored-by: Alex Johnson <alex.johnson293@gmail.com>
    Co-authored-by: Daniel Yakovlev <daniel.yakovlev@gmail.com>
    Co-authored-by: Yodel Guanzon <yodelguanzon@gmail.com>

--- src/js/rpg.js
+++ src/js/rpg.js
@@ -29,46 +29,46 @@ const changeStateString = (prop) => {
   }
 }
 
-// add character attribute functions
-// const addActive = changeState("active")(1);
-// const addGoofball = changeState("goofball")(1);
-// const addCreative = changeState("creative")(1);
-// const addGenius = changeState("genius")(1);
-// const addSelfAssured = changeState("selfAssured")(1);
-// const addMean = changeState("mean")(1);
-// const addBeginnerPoints = changeState("BeginnerPoints")(1);
+// set init point pool at 20
+const initPointPool = { ...stateControl(changeStateNum("charPoints")(20)) };
 
 
 // UI logic
 function addPoints() {
-  // document.getElementById("add-active").onclick = () => {
-  //   // removed stateControl from add variables, call stateControl here b/c reasons. also add to beginner points
-  //   const newState = { ...stateControl(addActive), ...stateControl(addBeginnerPoints) };
-  //   document.getElementById("active").innerText = `${newState.active}`;
-  // }
-
   document.getElementById("add-points").onclick = (event) => {
     if (event.target.id === "add-points") {
       return;
     }
-    const attribute = event.target.value;
-    const newState = { ...stateControl(changeStateNum(attribute)(1)) }
-    document.getElementById(attribute).innerText = `${newState[attribute]}`;
+    // check to see if enough points
+    const charNow = stateControl();
+    const ptsNow = charNow.charPoints;
+    if (ptsNow > 0) {
+      const attribute = event.target.value;
+      const newState = { ...stateControl(changeStateNum(attribute)(1)) }
+      // reduce charPoints by one
+      const newPts = { ...stateControl(changeStateNum("charPoints")(-1)) };
+      document.getElementById(attribute).innerText = `${newState[attribute]}`;
+      document.getElementById("points-now").innerText = `Points: ${newPts.charPoints}`;
+    } else {
+      document.getElementById("points-now").innerText = `Points: ${charNow.charPoints} You've spent all your points!` ;
+    }
   }
 }
 
 window.onload = function() {
-  // e.preventDefault();
   document.getElementById("create-char").onsubmit = (e) => {
     e.preventDefault();
-    // change innerText to .value
+
     let charName = document.getElementById("char-name").value;
     const character = { ...stateControl(changeStateString("name")(charName)) };
-    document.getElementById("name").innerText = `Name: ${character.name}`;
+    // give character 20 points to start
+    initPointPool;
+    document.getElementById("name").innerText = `Name: ${character.name} Points: ${character.charPoints}`;
   }
-
+  
   addPoints();
 
+  // show character stats
   document.getElementById("finalize").onclick = () => {
     const newState = { ...stateControl() };
     document.getElementById("character-name").innerText = `${newState.name}`;
